57064f0387ad14b31c2a705bb13502b65514f2b5
TvT <toantran114477@gmail.com>
2025-05-05 23:16:30 +0700

    Feat: Add GetCurrentUser Controller

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -55,3 +55,25 @@ export const login = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Không có token, truy cập bị từ chối" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "Người dùng không tồn tại" });
+    }
+
+    res.json({ id: user._id, email: user.email, username: user.username });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Token không hợp lệ" });
+  }
+};
